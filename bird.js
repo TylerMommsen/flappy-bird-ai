f9c70a9b82d16b65f7b85ce84e21b47734e756d3
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2024-05-06 16:17:38 +0300

    Add bird rotation

--- bird.js
+++ bird.js
@@ -8,6 +8,7 @@ class Bird {
 		this.gravity = 1;
 		this.flapPower = -17;
 		this.flapSound = flapSound;
+		this.fallRotation = -PI / 6;
 
 		// move bird down
 		this.move = () => {
@@ -23,7 +24,27 @@ class Bird {
 	}
 
 	show(img) {
-		image(img, this.x, this.y, this.width, this.height);
+		push();
+		// move the origin to the bird's center
+		translate(this.x + this.width / 2, this.y + this.height / 2);
+
+		// determine rotation based on velocity
+		if (this.velocity < 15) {
+			this.fallRotation = -PI / 6; // upward angle
+		} else if (this.velocity <= 25) {
+			this.fallRotation += PI / 16; // downward angle
+			this.fallRotation = constrain(this.fallRotation, -PI / 6, PI / 2);
+		} else {
+			this.fallRotation = PI / 2; // maximum downward angle
+		}
+
+		// apply the rotation
+		rotate(this.fallRotation);
+
+		// draw the bird image, centered on the translated origin
+		imageMode(CENTER);
+		image(img, 0, 0, this.width, this.height);
+		pop();
 	}
 
 	update() {
